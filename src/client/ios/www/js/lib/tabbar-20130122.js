48aaa13aa83e675b0581438d54712f1f3310367c
Chris Kuehl <chris@techxonline.net>
2013-01-22 12:16:29 -0500

    Cleaning up tab bar script.

--- src/client/ios/www/js/lib/tabbar-20130122.js
+++ src/client/ios/www/js/lib/tabbar-20130122.js
@@ -151,12 +151,9 @@ TabBar.prototype.updateItem = function(name, options) {
 
 cordova.addConstructor(function()
 {
-	console.log("here goes");
 	if(!window.plugins)
 		window.plugins = {};
 
     window.plugins.tabBar = new TabBar();
-    
-    console.log("PLUGINS: " + JSON.stringify(window.plugins));
     
 });
