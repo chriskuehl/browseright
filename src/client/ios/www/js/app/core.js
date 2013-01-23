9433ee7a423c5667eec13e588afe337874229d60
Chris Kuehl <chris@techxonline.net>
2013-01-23 10:12:28 -0500

    Adding additional core dependency and adjusting gui.

--- src/client/ios/www/js/app/core.js
+++ src/client/ios/www/js/app/core.js
@@ -1,4 +1,14 @@
 // handles core features (e.g. logging)
+function dd(str) {
+	str = str.toString();
+	
+	if (str.length < 2) {
+		return "0" + str;
+	}
+	
+	return str;
+}
+
 function log(msg) {
 	var now = new Date();
 	msg = "[" + dd(now.getHours()) + ":" + dd(now.getMinutes()) + ":" + dd(now.getSeconds()) + "]: " + msg;
