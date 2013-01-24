5f07af908dbc2f917fa974908f37c3ffe42e76b9
Chris Kuehl <chris@techxonline.net>
2013-01-24 10:13:26 -0500

    Adding beginnings of screen and transition system.

--- src/client/ios/www/js/app/gui.js
+++ src/client/ios/www/js/app/gui.js
@@ -1,8 +1,13 @@
+var gui = {};
+
 function initInterface() {
 	log("Initializing interface");
 	
 	applyInterfaceTweaks();
 	initTabBar();
+	
+	initScreenHolder();
+	setScreen("start/login");
 }
 
 function applyInterfaceTweaks() {
@@ -18,4 +23,48 @@ function initTabBar() {
 	
 	plugins.tabBar.show();
 	plugins.tabBar.showItems("contacts", "recents");
+}
+
+function initScreenHolder() {
+	resetScreen();
+}
+
+function resetScreen() {
+	if (! gui.currentScreen) {
+		return;
+	}
+	
+	gui.oldScreen = gui.currentScreen;
+	gui.currentScreen = null;
+}
+
+function setScreen(screenPath) {
+	resetScreen();
+	
+	// create a new screen container
+	var screenContainer = createNewScreenContainer();
+	
+	showNewScreen(function() {
+		
+	});
+}
+
+function showNewScreen(callback) {
+	if (! gui.oldScreen) {
+		gui.currentScreen.show();
+		gui.currentScreen.css({
+			left: "0px"
+		});
+		
+		callback();
+		return;
+	}
+}
+
+function createNewScreenContainer() {
+	var container = $("<div />");
+	container.addClass("screenContainer");
+	container.appendTo($("#contentHolder"));
+	
+	return container;
 }
