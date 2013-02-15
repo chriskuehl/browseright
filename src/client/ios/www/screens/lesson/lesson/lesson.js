823d4cfb7f4c6bb4e5a4f15f2d0b8739071cc3a8
Chris Kuehl <chris@techxonline.net>
2013-02-15 14:06:07 -0500

    Establishing format for storing navBar data.

--- src/client/ios/www/screens/lesson/lesson/lesson.js
+++ src/client/ios/www/screens/lesson/lesson/lesson.js
@@ -1,5 +1,15 @@
 gui.screens["lesson/lesson"].data = {
 	id: "lesson/lesson",
+	navBars: [
+		[
+			title: "Lesson One",
+			width: 1700
+		],
+		
+		[
+			title: "Progress"
+		]
+	],
 	
 	setup: function(contentManager) {
 		
