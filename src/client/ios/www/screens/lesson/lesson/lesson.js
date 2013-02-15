b57897e68b2d5dae96f5d8239bb158618cfcd16f
Chris Kuehl <chris@techxonline.net>
2013-02-15 14:21:34 -0500

    Fixing bug caused by Caleb's impatience.

--- src/client/ios/www/screens/lesson/lesson/lesson.js
+++ src/client/ios/www/screens/lesson/lesson/lesson.js
@@ -1,14 +1,14 @@
 gui.screens["lesson/lesson"].data = {
 	id: "lesson/lesson",
 	navBars: [
-		[
+		{
 			title: "Lesson One",
 			width: 1700
-		],
+		},
 		
-		[
+		{
 			title: "Progress"
-		]
+		}
 	],
 	
 	setup: function(contentManager) {
