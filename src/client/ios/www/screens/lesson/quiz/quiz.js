65d390c242cc44f8fff54af08d7ae02c7156b29b
Caleb Dotson <pylonmedia@gmail.com>
2013-02-18 09:37:27 -0500

    Changing nav list and adding quiz page.

---
+++ src/client/ios/www/screens/lesson/quiz/quiz.js
@@ -0,0 +1,17 @@
+gui.screens["lesson/quiz"].data = {
+	id: "lesson/quiz",
+	navBars: [
+		{
+			title: "Lesson One",
+			width: 1700
+		},
+		
+		{
+			title: "Progress"
+		}
+	],
+	
+	setup: function(contentManager) {
+		
+	}
+};
