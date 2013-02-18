36346d8e65b135be2949b805dac68e9d988ae539
Caleb Dotson <pylonmedia@gmail.com>
2013-02-18 11:18:15 -0500

    Renaming icon and adding temporary navigation.

--- src/client/ios/www/js/app/gui.js
+++ src/client/ios/www/js/app/gui.js
@@ -36,12 +36,12 @@ function initTabBar() {
     });
     plugins.tabBar.createItem("progress", "Progress", "/www/css/assets/tabbar/progress.png", {
 		onSelect: function() {
-			setScreen("test/one");
+			setScreen("lesson/quiz");
 		}
 	});
     plugins.tabBar.createItem("settings", "Settings", "/www/css/assets/tabbar/settings.png", {
 		onSelect: function() {
-			setScreen("test/one");
+			setScreen("lesson/lesson");
 		}
 	});
     
