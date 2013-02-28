6a3ee0c16a5acd75c1313ccedfcfff403f83b1fd
Chris Kuehl <chris@techxonline.net>
2013-02-28 11:29:08 -0500

    Changes to app core.

--- src/client/ios/www/js/app/core.js
+++ src/client/ios/www/js/app/core.js
@@ -1,4 +1,56 @@
-// handles core features (e.g. logging)
+// handles core features (e.g. logging, device management)
+
+// device management and state
+var ready = false;
+var online = false;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("online", onDeviceOnline, false);
+document.addEventListener("offline", onDeviceOffline, false);
+document.addEventListener("pause", onDevicePaused, false);
+document.addEventListener("resume", onDeviceResumed, false);
+
+function onDeviceReady() {
+	log("Device is ready.");
+	
+	ready = true;
+	attemptInitialization();
+}
+
+function onDeviceOnline() {
+	log("Device is online.");
+	
+	online = true;
+	attemptInitialization();
+}
+
+function onDeviceOffline() {
+	log("Device is offline.");
+	
+	online = false;
+	attemptInitialization();
+}
+
+function onDevicePaused() {
+	log("Device has been paused.");
+}
+
+function onDeviceResumed() {
+	log("Device has been resumed.");
+}
+
+function attemptInitialization() {
+	// TODO: initialize
+}
+
+// check user agent for testing on PC
+if (PLATFORM == PLATFORM_PC) {
+	// simulate normal events
+	onDeviceReady();
+	onDeviceOnline();
+}
+
+// logging
 var logRecord = "Welcome to the BrowseRight console.";
 
 function dd(str) {
