84fb48ecf3e658c710eec18e5996d76793427f80
Chris Kuehl <chris@techxonline.net>
2013-02-28 10:17:52 -0500

    Adding user.js for handling user interaction and basing start page on login status.

--- src/client/ios/www/js/app/start.js
+++ src/client/ios/www/js/app/start.js
@@ -1,4 +1,10 @@
 // first action after loading (can be overwritten by a server-loaded JS file, which is why this is separated)
 function startApp() {
-	setScreen("dev/console");
+	if (isLoggedIn()) {
+		setScreen("lesson/category");
+	} else {
+		setScreen("start/login");
+	}
+	
+	// setScreen("dev/console");
 }
