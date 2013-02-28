4df379546fa9f2d64f949f0f672815ddc61f85db
Caleb Dotson <pylonmedia@gmail.com>
2013-02-28 10:30:46 -0500

    Removing ghost of testing past.

--- src/client/ios/www/screens/start/login/login.js
+++ src/client/ios/www/screens/start/login/login.js
@@ -9,7 +9,7 @@ gui.screens["start/login"].data = {
 		
 	setup: function(contentManager) {
 		$(".login").click(function() {
-			setScreen("test/one");
+			setScreen("lesson/category");
 		});
 	}
 };
