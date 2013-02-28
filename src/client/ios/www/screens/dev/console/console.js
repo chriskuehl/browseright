d8cf9903d2a8f10c24e188fcd89483be80ae8996
Chris Kuehl <chris@techxonline.net>
2013-02-28 10:17:24 -0500

    Console changes to preserve text.

--- src/client/ios/www/screens/dev/console/console.js
+++ src/client/ios/www/screens/dev/console/console.js
@@ -7,6 +7,9 @@ gui.screens["dev/console"].data = {
 	],
 	
 	setup: function(contentManager) {
+		var logs = $(".logContainer");
 		
+		logs.html(logRecord);
+		logs.scrollTop(logs[0].scrollHeight);
 	}
 };
