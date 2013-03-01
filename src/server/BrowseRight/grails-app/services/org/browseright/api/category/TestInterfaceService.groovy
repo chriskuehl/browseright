fdeffeba2d99634151b188345f8aa0639796be7d
Chris Kuehl <chris@techxonline.net>
2013-03-01 12:23:26 -0500

    Adjusting TestInterfaceService for testing.

--- src/server/BrowseRight/grails-app/services/org/browseright/api/category/TestInterfaceService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/api/category/TestInterfaceService.groovy
@@ -1,16 +1,7 @@
 package org.browseright.api.category
 
 class TestInterfaceService {
-    def report = { response, action, params, user ->
-        def email = params.email
-        def name = params.name
-        def comments = params.comments
-
-        def body = "From: ${name} <${email}>\n\n"
-        body += comments + "\n\n"
-        body += "User Agent: ${params.info.userAgent}\n"
-        body += "IP Address: ${params.info.ip}"
-
-        emailService.sendMail("chris@trenders.org", "Comments from ${name}", body)
+    def test = { response, action, params, user ->
+        response.itworked = true
     }
 }
