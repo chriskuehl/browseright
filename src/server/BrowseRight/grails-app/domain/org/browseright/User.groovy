67eadd463effab9cb08a56a8d141cfbe79887e8d
Chris Kuehl <chris@techxonline.net>
2013-03-05 12:09:22 -0500

    Adding logic in User class for password hashing and setting login/registration times/IPs/user agents.

--- src/server/BrowseRight/grails-app/domain/org/browseright/User.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/User.groovy
@@ -1,6 +1,8 @@
 package org.browseright
 
 abstract class User {
+    def hashingService
+    
     static constraints = {
 	email(email: true, unique: true)
 	school(nullable: true)
@@ -28,4 +30,20 @@ abstract class User {
 
     // BR-specific stuff
     School school
+    
+    def setPassword(def password) {
+	passwordHash = hashingService.hash(password)
+    }
+    
+    def setLastSeen(request) {
+	lastSeenIP = request.getHeader("X-Forwarded-For") ? request.getHeader("X-Forwarded-For") : "none"
+	lastSeenUserAgent = request.getHeader("User-Agent") ? request.getHeader("User-Agent") : "none"
+	lastSeenTime = new Date()
+    }
+    
+    def setRegistration(request) {
+	registerIP = request.getHeader("X-Forwarded-For") ? request.getHeader("X-Forwarded-For") : "none"
+	registerUserAgent = request.getHeader("User-Agent") ? request.getHeader("User-Agent") : "none"
+	registerTime = new Date()
+    }
 }
