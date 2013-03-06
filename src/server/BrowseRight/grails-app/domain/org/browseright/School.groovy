b3e77dc441dcb6a29cf2f9b38ab6228101f6945f
Chris Kuehl <chris@techxonline.net>
2013-03-06 10:28:31 -0500

    Updating School to add some methods for generating classroom codes and other creation details.

--- src/server/BrowseRight/grails-app/domain/org/browseright/School.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/School.groovy
@@ -1,6 +1,8 @@
 package org.browseright
 
 class School {
+    def schoolService
+    
     static constraints = {
 	registerCode(unique: true)
 	helpEmail(email: true)
@@ -28,4 +30,32 @@ class School {
     String helpEmail
     String announcementText = "Welcome to BrowseRight!"
     String helpText = "For help, please contact your school."
+    
+    def School() {
+        generateNewRegisterCode()
+    }
+    
+    def setLocation(def street, def city, def zipCode, def state) {
+        // TODO: find latitude and longitude for location
+        this.street = street
+        this.city = city
+        this.zipCode = zipCode
+        this.state = state
+    }
+    
+    def updateProgress() {
+        // TODO: update progress caches for all students
+    }
+    
+    def generateNewRegisterCode() {
+        registerCode = schoolService.generateUnqiueCode()
+    }
+    
+    def addStudent(Student student) {
+        students.add(student)
+    }
+    
+    def addTeacher(Teacher teacher) {
+        teachers.add(teacher)
+    }
 }
