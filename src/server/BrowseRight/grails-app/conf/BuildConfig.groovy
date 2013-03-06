465e554a1ae71464fe7bbe980d3b31ecd2d61a91
Chris Kuehl <chris@techxonline.net>
2013-03-06 12:11:54 -0500

    Cleaning up BuildConfig a little.

--- src/server/BrowseRight/grails-app/conf/BuildConfig.groovy
+++ src/server/BrowseRight/grails-app/conf/BuildConfig.groovy
@@ -45,18 +45,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":hibernate:$grailsVersion"
-        // runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
-
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.5"
-
         build ":tomcat:$grailsVersion"
-
-        // runtime ":database-migration:1.3.2"
-
-        // compile ':cache:1.0.1'
     }
 }
