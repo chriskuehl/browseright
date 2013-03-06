87526267cf653d7ea179ed6670f7f2b25ed7e21a
Chris Kuehl <chris@techxonline.net>
2013-03-06 12:41:40 -0500

    Adding GeolocationService for getting state from zip code.

---
+++ src/server/BrowseRight/grails-app/services/org/browseright/GeolocationService.groovy
@@ -0,0 +1,8 @@
+package org.browseright
+
+class GeolocationService {
+    def findStateByZipCode(def zipCode) {
+        // TODO: this
+        "unknown"
+    }
+}
---
+++ src/server/BrowseRight/test/unit/org/browseright/GeolocationServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.browseright
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(GeolocationService)
+class GeolocationServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
