d1ed4d1c54e9cec441540be43bd2c37dffecdb99
Chris Kuehl <chris@techxonline.net>
2013-03-07 10:06:29 -0500

    Executing a few API methods on bootstrap to insert test data into the database.

--- src/server/BrowseRight/grails-app/conf/BootStrap.groovy
+++ src/server/BrowseRight/grails-app/conf/BootStrap.groovy
@@ -1,8 +1,14 @@
 class BootStrap {
     def geolocationService
+    def appInterfaceService
     
     def init = { servletContext ->
 	geolocationService.initZipCodeDatabase()
+	
+	// execute some API methods to add some data for testing
+	appInterfaceService.generateResponse(null, "teacher", "create", null, [firstName: "Some", lastName: "Teacher", email: "teacher.test@browseright.org", password: "teacher"])
+	appInterfaceService.generateResponse(null, "school", "create", null, [name: "Fake High School", street: "127 Lemon Drive", city: "Iowa City", zipCode: "52246", helpEmail: "help@browseright.org"])
+	
     }
     def destroy = {
     }
