f25db393bcf26fbf0a523adb0fbfeadf143022e2
Chris Kuehl <chris@techxonline.net>
2013-03-07 10:36:10 -0500

    Whitespace cleanup.

--- src/server/BrowseRight/grails-app/conf/DataSource.groovy
+++ src/server/BrowseRight/grails-app/conf/DataSource.groovy
@@ -9,6 +9,7 @@ hibernate {
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
+
 // environment specific settings
 environments {
     development {
