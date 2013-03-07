f78489ea763c025335c2667cf7702d1475b11962
Chris Kuehl <chris@techxonline.net>
2013-03-07 13:48:26 -0500

    Modifying UserSession to generate its own token, plus removing last-seen URL.

--- src/server/BrowseRight/grails-app/domain/org/browseright/UserSession.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/UserSession.groovy
@@ -1,12 +1,16 @@
 package org.browseright
 
 class UserSession {
+    def hashingService
+    
     static constraints = {
 	lastSeenIP(nullable: true)
 	lastSeenTime(nullable: true)
 	lastSeenUserAgent(nullable: true)
-	lastSeenURL(nullable: true)
     }
+    
+    static belongsTo = [user: User]
+    static transients = ["hashingService"]
     
     User user
     String sessionTokenHash
@@ -15,5 +19,32 @@ class UserSession {
     String lastSeenIP
     Date lastSeenTime
     String lastSeenUserAgent
-    String lastSeenURL
+    
+    def generateNewToken() {
+        def chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#%^&*()-=+_[]{}|,./?><`~"
+	def str = ""
+        
+        (0..255).each {
+            str += chars.charAt((int) Math.floor(Math.random() * chars.size()))
+        }
+	
+	// session tokens are basically password equivalents, so they get hashed
+	// like passwords
+	sessionTokenHash = hashingService.hash(str)
+        
+	// return the non-hashed token, which is then given to the user
+        str
+    }
+    
+    def setLastSeen(request) {
+	if (request != null) {
+	    lastSeenIP = request.getHeader("X-Forwarded-For") ? request.getHeader("X-Forwarded-For") : request.getRemoteAddr()
+	    lastSeenUserAgent = request.getHeader("User-Agent") ? request.getHeader("User-Agent") : "none"
+	} else {
+	    lastSeenIP = "none"
+	    lastSeenUserAgent = "none"
+	}
+	
+	lastSeenTime = new Date()
+    }
 }
