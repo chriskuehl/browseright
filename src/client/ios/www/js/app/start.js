9e5bc7e52cbf27b02707dbcdd14f23edd8230207
Chris Kuehl <chris@techxonline.net>
2013-03-08 15:06:17 -0500

    Changes to start.js to properly set screen based on login status.

--- src/client/ios/www/js/app/start.js
+++ src/client/ios/www/js/app/start.js
@@ -1,9 +1,10 @@
 // first action after loading (can be overwritten by a server-loaded JS file, which is why this is separated)
 function startApp() {
-	if (true || isLoggedIn()) {
-		setScreen("lesson/category");
-	} else {
+	if (false && isLoggedIn()) {
 		loadStudentData();
+	//	setScreen("lesson/category");
+	} else {
+		setScreen("start/login");
 	}
 	
 	//api("test/test", {fromApp: true}, [RESP_OK], function(status, data) {
