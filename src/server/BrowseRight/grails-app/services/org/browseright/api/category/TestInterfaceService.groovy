d5026887de9d96f3c0c3eddf5a94d28214b3fee2
Chris Kuehl <chris@techxonline.net>
2013-03-08 11:59:49 -0500

    Adding test/whoami API command to see who is logged in.

--- src/server/BrowseRight/grails-app/services/org/browseright/api/category/TestInterfaceService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/api/category/TestInterfaceService.groovy
@@ -1,7 +1,16 @@
 package org.browseright.api.category
 
+import org.browseright.*
+
 class TestInterfaceService {
     def test = { response, action, params, user, request ->
         response.itworked = true
+    }
+    
+    def whoami = { response, action, params, user, request ->
+	response.youare = [
+	    teacher: (user instanceof Teacher),
+	    email: user.email
+	]
     }
 }
