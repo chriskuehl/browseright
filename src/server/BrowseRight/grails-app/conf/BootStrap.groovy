d4a542497e059900d643c130073ac49ec056b267
Chris Kuehl <chris@techxonline.net>
2013-03-08 12:14:30 -0500

    Now creating a user session with token "0"x255 on bootstrap for testing.

--- src/server/BrowseRight/grails-app/conf/BootStrap.groovy
+++ src/server/BrowseRight/grails-app/conf/BootStrap.groovy
@@ -1,3 +1,5 @@
+import org.browseright.*
+
 class BootStrap {
     def geolocationService
     def appInterfaceService
@@ -10,7 +12,14 @@ class BootStrap {
 	appInterfaceService.generateResponse(null, "school", "create", null, [name: "Fake High School", street: "127 Lemon Drive", city: "Iowa City", zipCode: "52246", helpEmail: "help@browseright.org"])
 	
 	appInterfaceService.generateResponse(null, "student", "create", null, [firstName: "Some", lastName: "Student", email: "student.test@browseright.org", password: "student"])
+	
+	// login once, then change the token to something we can easily test with
+	def resp = appInterfaceService.generateResponse(null, "student", "login", null, [email: "student.test@browseright.org", password: "student"])
+	def session = UserSession.findByToken(resp.token)
+	session.setToken("0" * 255)
+	session.save(flush: true)
     }
+    
     def destroy = {
     }
 }
