fb232882e873030810b9ef4ba72c644426ada7eb
Chris Kuehl <chris@techxonline.net>
2013-03-08 23:56:21 -0500

    Now loading test/whoami when getting user information.

--- src/client/ios/www/js/app/user.js
+++ src/client/ios/www/js/app/user.js
@@ -1,7 +1,8 @@
 function loadStudentData() {
-	//apiWithLoading("Loading student data...", "student/login", {}, [RESP_OK], function(code, data) {
-	//	setScreen("start/login");
-	//});
+	apiWithLoading("Loading student data...", "test/whoami", {}, [RESP_OK], function(code, data) {
+		alert(JSON.stringify(data));
+		setScreen("start/login");
+	});
 }
 
 function isLoggedIn() {
