67610d482cd9617c591afe122f5debd0a9261a2e
Chris Kuehl <chris@techxonline.net>
2013-03-08 23:53:53 -0500

    Using loading screen for login now.

--- src/client/ios/www/screens/start/login/login.js
+++ src/client/ios/www/screens/start/login/login.js
@@ -12,13 +12,13 @@ gui.screens["start/login"].data = {
 			var email = $(".email").val();
 			var password = $(".password").val();
 			
-			api("student/login", {email: email, password: password}, [RESP_OK, RESP_BAD_LOGIN_INFO], function(code, data) {
+			apiWithLoading("Logging in...", "student/login", {email: email, password: password}, [RESP_OK, RESP_BAD_LOGIN_INFO], function(code, data) {
 				if (code == RESP_OK) {
 					localStorage["userToken"] = data.token;
 					loadStudentData();					
 				} else if (code == RESP_BAD_LOGIN_INFO) {
 					if (data.error == "PASSWORD") {
-						dialog("Incorrect Password", "Your email is right, but that's the wrong password! Try again?", ["Oops!"]);
+						dialog("Incorrect Password", "Your email is right, but that's the wrong password! Make sure you're using a student account (not a teacher account). Try again?", ["Oops!"]);
 					} else if (data.error == "EMAIL") {
 						dialog("Incorrect Email", "We don't recognize that email address. Try again?", ["Oops!"]);
 					} else {
