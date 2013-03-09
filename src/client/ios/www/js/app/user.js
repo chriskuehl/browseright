87c25ca7081dacdcdcb3703ae8e37444c1954c94
Chris Kuehl <chris@techxonline.net>
2013-03-08 23:57:40 -0500

    Sending student to portal page on login.

--- src/client/ios/www/js/app/user.js
+++ src/client/ios/www/js/app/user.js
@@ -1,7 +1,6 @@
 function loadStudentData() {
 	apiWithLoading("Loading student data...", "test/whoami", {}, [RESP_OK], function(code, data) {
-		alert(JSON.stringify(data));
-		setScreen("start/login");
+		setScreen("user/portal");
 	});
 }
 
