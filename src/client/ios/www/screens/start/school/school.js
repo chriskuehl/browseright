beb2ff44d554938ad451751354f632a88d8d3bbd
Chris Kuehl <chris@techxonline.net>
2013-03-11 11:29:57 -0400

    Reducing number of test schools.

--- src/client/ios/www/screens/start/school/school.js
+++ src/client/ios/www/screens/start/school/school.js
@@ -20,7 +20,7 @@ gui.screens["start/school"].data = {
     parents: ["start/login"],
 		
 	setup: function(contentManager) {
-		for (var i = 0; i < 100; i ++) {
+		for (var i = 0; i < 20; i ++) {
 			addSchool(i, "School #" + i, i + " Some Street", "Middle of Nowhere, IA 52246");
 		}
 		
