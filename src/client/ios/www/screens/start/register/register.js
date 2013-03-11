e775c83a3ebeee2d1295a6f0c13171a36afbc357
Caleb Dotson <pylonmedia@gmail.com>
2013-03-11 10:12:40 -0400

    Adding student account creation.

--- src/client/ios/www/screens/start/register/register.js
+++ src/client/ios/www/screens/start/register/register.js
@@ -28,10 +28,22 @@ gui.screens["start/register"].data = {
 			var emailC = $(".confirmEmail").val();
 			var password = $(".password").val();
 			var passwordC = $(".confirmPassword").val();
+
+			if (email != emailC) {
+				return dialog("Make sure your email matches.", ["OK"]);
+			}
 			
+			if (password != passwordC) {
+				return dialog("Make sure your password matches.", ["OK"]);
+			}
 			
-			
-			setScreen("user/portal");
+			apiWithLoading("Registering account...", "student/create", {firstName: first, lastName: last, email: email, password: password}, [RESP_OK, RESP_MISSING_BAD_PARAMS], function(code, data) {
+				if (code == RESP_OK) {
+					setScreen("user/portal");					
+				} else if (code == RESP_MISSING_BAD_PARAMS) {
+					dialog(data.error, ["OK"]);
+				}
+			});
 		});
 	}
 };
