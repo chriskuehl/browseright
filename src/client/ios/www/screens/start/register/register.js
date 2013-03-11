13d49c84ff58b350b96867e5a15642abc18a44a0
Caleb Dotson <pylonmedia@gmail.com>
2013-03-11 09:44:36 -0400

    Storing vars on register page input.

--- src/client/ios/www/screens/start/register/register.js
+++ src/client/ios/www/screens/start/register/register.js
@@ -21,6 +21,16 @@ gui.screens["start/register"].data = {
 		
 	setup: function(contentManager) {
 		$(".create").click(function() {
+				
+			var first = $(".first").val();
+			var last = $(".last").val();
+			var email = $(".email").val();
+			var emailC = $(".confirmEmail").val();
+			var password = $(".password").val();
+			var passwordC = $(".confirmPassword").val();
+			
+			
+			
 			setScreen("user/portal");
 		});
 	}
