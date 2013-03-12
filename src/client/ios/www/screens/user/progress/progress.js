7d9650e4ee64d9fe6930e926dce87fa1d2b071fe
Caleb Dotson <pylonmedia@gmail.com>
2013-03-12 12:16:39 -0400

    Started adding in function for generating the progress categories.

--- src/client/ios/www/screens/user/progress/progress.js
+++ src/client/ios/www/screens/user/progress/progress.js
@@ -12,3 +12,14 @@ gui.screens["user/progress"].data = {
     	registerScrollContainers($(".scroll"));
     }
 };
+
+function addSection(category, completed, street1, street2) {
+	$("<div />").addClass(".category");
+	$("<div />").addClass(".title").text(category);
+	var ul = $(".category").find("ul");
+	var li = $("<li />");
+	$("<li />").addClass("section").text(street1).appendTo(a);
+	$("<p />").addClass("address").text(street2).appendTo(a);
+	
+	li.appendTo(ul);
+}
