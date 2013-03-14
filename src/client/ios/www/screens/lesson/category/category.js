eb0c0b88a7846b841b7ad92186746a7020640b57
Chris Kuehl <chris@techxonline.net>
2013-03-14 10:09:05 -0400

    Now loading categories from the API.

--- src/client/ios/www/screens/lesson/category/category.js
+++ src/client/ios/www/screens/lesson/category/category.js
@@ -5,8 +5,29 @@ gui.screens["lesson/category"].data = {
 	}],
 
 	setup: function(contentManager) {
-		$($(".category")[0]).click(function() {
-			setScreen("lesson/lesson");
+		apiWithLoading("Loading lesson categories...", "content/categories", {}, [RESP_OK], function(code, data) {
+			for (var i = 0; i < data.categories.length; i ++) {
+				addCategory(i, data.categories[i]);
+			}
 		});
 	}
 };
+
+/*
+
+<div class="category">
+    <div class="text"><h3>Category One</h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit feugiat aliquam condimcentum. Fusce
+    </div>
+</div>
+
+*/
+
+function addCategory(i, category) {
+	var div = $("<div />").addClass("category").addClass("category-" + i);
+	var text = $("<div />").addClass("text").appendTo(div);
+	$("<h3 />").text(category.title).appendTo(text);
+	$("<p />").text(category.shortDescription).appendTo(text);
+	
+	div.appendTo($(".categoryHolder"));
+}
