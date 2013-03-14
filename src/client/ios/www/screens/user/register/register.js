989b9fb8c41ccfd986feb3ac0e895ade696afac9
Chris Kuehl <chris@techxonline.net>
2013-03-14 00:20:01 -0400

    Whitespace cleanup on registration page.

--- src/client/ios/www/screens/user/register/register.js
+++ src/client/ios/www/screens/user/register/register.js
@@ -1,33 +1,31 @@
 gui.screens["user/register"].data = {
 	id: "user/register",
 	hideTabBar: true,
-    navBars: [
-        {
-            title: "Create an Account",
-            
-            buttons: {
-				left: {
-					type: "back",
-					title: "Select a School",
-					action: function() {
-						setScreen("user/school");
-					}
+	navBars: [{
+		title: "Create an Account",
+
+		buttons: {
+			left: {
+				type: "back",
+				title: "Select a School",
+				action: function () {
+					setScreen("user/school");
 				}
 			}
-        }
-    ],
-    
-    parents: ["user/school"],
-		
-	setup: function(contentManager) {
+		}
+	}],
+
+	parents: ["user/school"],
+
+	setup: function (contentManager) {
 		if (selectedSchool == null) {
-			dialog("Select a School", "Please select a school first!", ["OK"], function() {
+			dialog("Select a School", "Please select a school first!", ["OK"], function () {
 				setScreen("user/login");
 			});
-			
+
 			return;
 		}
-		
+
 		// login by enter on input fields
 		$("input").keypress(function (e) {
 			if (e.which == 13) {
@@ -38,11 +36,10 @@ gui.screens["user/register"].data = {
 				return false;
 			}
 		});
-		
+
 		$(".schoolUN").val(selectedSchool.name);
-		
-		$(".create").click(function() {
-				
+
+		$(".create").click(function () {
 			var first = $(".first").val();
 			var last = $(".last").val();
 			var email = $(".email").val();
@@ -53,14 +50,19 @@ gui.screens["user/register"].data = {
 			if (email != emailC) {
 				return dialog("Confirm Email", "Make sure your email matches.", ["OK"]);
 			}
-			
+
 			if (password != passwordC) {
 				return dialog("Confirm Password", "Make sure your password matches.", ["OK"]);
 			}
-			
-			apiWithLoading("Registering account...", "student/create", {firstName: first, lastName: last, email: email, password: password}, [RESP_OK, RESP_MISSING_BAD_PARAMS], function(code, data) {
+
+			apiWithLoading("Registering account...", "student/create", {
+				firstName: first,
+				lastName: last,
+				email: email,
+				password: password
+			}, [RESP_OK, RESP_MISSING_BAD_PARAMS], function (code, data) {
 				if (code == RESP_OK) {
-					setScreen("user/portal");					
+					setScreen("user/portal");
 				} else if (code == RESP_MISSING_BAD_PARAMS) {
 					dialog("Registration Error", "The " + errorToEnglish(data.error, " you entered") + ".", ["OK"]);
 				}
