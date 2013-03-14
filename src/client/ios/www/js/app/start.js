7f07daa389be9b41ae3cdb7d26df4d46db712d63
Chris Kuehl <chris@techxonline.net>
2013-03-14 00:52:22 -0400

    Cleaning up start.js.

--- src/client/ios/www/js/app/start.js
+++ src/client/ios/www/js/app/start.js
@@ -1,16 +1,13 @@
 // first action after loading (can be overwritten by a server-loaded JS file, which is why this is separated)
 
 function startApp() {
-	if (false && isLoggedIn()) {
-		loadStudentData();
-		//	setScreen("lesson/category");
+	if (isLoggedIn()) {
+		setScreen("user/login");
+		
+		loadStudentData(function() {
+			setScreen("lesson/categories");
+		});
 	} else {
 		setScreen("user/login");
 	}
-
-	//api("test/test", {fromApp: true}, [RESP_OK], function(status, data) {
-	//	alert("Status [" + status + "], data: " + JSON.stringify(data));
-	//});
-
-	// setScreen("dev/console");
 }
