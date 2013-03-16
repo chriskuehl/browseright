f16df3ed9e49a6e645bf72bd5be6b3ba94026fde
Chris Kuehl <chris@techxonline.net>
2013-03-15 22:44:00 -0400

    Now calling contentService to create categories and other content at the start rather that creating it in BootStrap.

--- src/server/BrowseRight/grails-app/conf/BootStrap.groovy
+++ src/server/BrowseRight/grails-app/conf/BootStrap.groovy
@@ -1,20 +1,17 @@
 import org.browseright.*
 
 class BootStrap {
+    def contentService
     def geolocationService
     def appInterfaceService
     
     def init = { servletContext ->
         geolocationService.initZipCodeDatabase()
+        contentService.initContentDatabase()
 	
         // execute some API methods to add some data for testing
         appInterfaceService.generateResponse(null, "teacher", "create", null, [firstName: "Some", lastName: "Teacher", email: "teacher.test@browseright.org", password: "teacher"])
         appInterfaceService.generateResponse(null, "student", "create", null, [firstName: "Some", lastName: "Student", email: "student.test@browseright.org", password: "student"])
-	
-        // create some categories
-        def reputation = new Category(title: "Your Online Reputation", shortDescription: "rep", longDescription: "repppppp").save()
-        def health = new Category(title: "Technology and Your Health", shortDescription: "stay healthy", longDescription: "healthh").save()
-        def peers = new Category(title: "Online Relationships", shortDescription: "how 2 relate online", longDescription: "onlineeeee").save()
 	
         // login once, then change the token to something we can easily test with
         def resp = appInterfaceService.generateResponse(null, "student", "login", null, [email: "student.test@browseright.org", password: "student"])
