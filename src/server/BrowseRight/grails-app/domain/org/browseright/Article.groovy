75362f4ca57aa69e759a24f667ef4f1e3852f10f
Chris Kuehl <chris@techxonline.net>
2013-03-15 23:25:09 -0400

    Changes to Article and Quiz parameters.

--- src/server/BrowseRight/grails-app/domain/org/browseright/Article.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/Article.groovy
@@ -4,7 +4,6 @@ class Article extends SectionItem {
     static constraints = {
 	text (maxSize: 8192)
     }
-    static belongsTo = [section: Section]
     
     String text
 }
--- src/server/BrowseRight/grails-app/domain/org/browseright/Quiz.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/Quiz.groovy
@@ -1,10 +1,6 @@
 package org.browseright
 
 class Quiz extends SectionItem {
-    static constraints = {
-	
-    }
-    
     static belongsTo = [section: Section]
     static hasMany = [questions: Question]
     
