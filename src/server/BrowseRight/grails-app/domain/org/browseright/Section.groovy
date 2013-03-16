b3777cf1a24173894a9393429340c22e653597e2
Chris Kuehl <chris@techxonline.net>
2013-03-15 23:26:12 -0400

    Adding UID (unique per category) to Section.

--- src/server/BrowseRight/grails-app/domain/org/browseright/Section.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/Section.groovy
@@ -2,10 +2,13 @@ package org.browseright
 
 class Section {
     static constraints = {
+        uid(unique: ["category"])
     }
     
+    static belongsTo = [category: org.browseright.Category]
     static hasMany = [items: SectionItem]
     
+    String uid
     String title
     String description
 }
