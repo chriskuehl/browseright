73848f9a75e5fff5ff858f5323d37019484ce97c
Chris Kuehl <chris@techxonline.net>
2013-03-15 22:45:33 -0400

    Adding uid to Category.

--- src/server/BrowseRight/grails-app/domain/org/browseright/Category.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/Category.groovy
@@ -2,11 +2,13 @@ package org.browseright
 
 class Category {
     static constraints = {
+        uid(unique: true, nullable: false, blank: false)
 	longDescription(maxSize: 2048)
     }
     
     static hasMany = [sections: Section]
     
+    String uid
     String title
     String shortDescription
     String longDescription
