f935d87dfb2ce0a57be1565a29b7e5dc2b5a09ba
Chris Kuehl <chris@techxonline.net>
2013-03-16 00:42:19 -0400

    Adding content/category API command for listing all sections in one category.

--- src/server/BrowseRight/grails-app/services/org/browseright/api/category/ContentInterfaceService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/api/category/ContentInterfaceService.groovy
@@ -1,7 +1,7 @@
 package org.browseright.api.category
 
-import browseright.*
-import browseright.api.*
+import org.browseright.*
+import org.browseright.api.*
 
 class ContentInterfaceService {
     def _categories = { response, action, params, user, request ->
@@ -20,20 +20,45 @@ class ContentInterfaceService {
         response.categories = categories
     }
     
-    def _sections = { response, action, params, user, request ->
-        def category = Category.findById(params.id)
+    def _category = { response, action, params, user, request ->
+        def category = Category.findByUid(params.uid)
         
-        if (category != null) {
+        if (category == null) {
 			response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
 			response.error = "NO_CATEGORY_EXISTS"
 			return
         }
         
-        
         def sections = []
         
         category.sections.each { section ->
-	   
+			def sectionData = [
+				uid: section.uid,
+				title: section.title,
+				description: section.description,
+				items: []
+			]
+			
+			section.items.each { item ->
+				def isQuiz = (item instanceof Quiz)
+				def itemData = [
+					id: item.id,
+					type: isQuiz ? "QUIZ" : "ARTICLE",
+					title: item.title
+				]
+				
+				if (isQuiz) {
+					// quiz
+				} else {
+					// article
+				}
+				
+				sectionData.items.add(itemData)
+			}
+			
+			sections.add(sectionData)
         }
+		
+		response.sections = sections
     }
 }
