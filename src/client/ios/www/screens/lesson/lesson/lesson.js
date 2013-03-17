d6ad153f59f7e0a13cd8dc83772e3a2fc0f82392
Chris Kuehl <chris@techxonline.net>
2013-03-17 00:48:41 -0400

    Loading section header into navigation.

--- src/client/ios/www/screens/lesson/lesson/lesson.js
+++ src/client/ios/www/screens/lesson/lesson/lesson.js
@@ -21,11 +21,26 @@ gui.screens["lesson/lesson"].data = {
 	parents: ["lesson/category"],
 
 	setup: function(contentManager) {
-		$(".lessonTitle").click(function() {
-			setScreen("lesson/category");
+		apiWithLoading("Loading lessons...", "content/category", {uid: selectedCategory}, [RESP_OK], function(code, data) {
+			$(".nav").empty();
+			
+			for (var i = 0; i < data.sections.length; i ++) {
+				addSection(data.sections[i]);
+			}
+			
+			$(".categoryHolder").fadeIn(250);
 		});
 
 		// TODO: Fix this
 		registerScrollContainers($(".scroll"));
 	}
 };
+
+function addSection(section) {
+	var header = $("<li />");
+	header.addClass("section");
+	header.appendTo($(".nav"));
+	var d = $("<div />");
+	d.appendTo(header);
+	d.text(section.title);
+}
