a6635ed6beddd7ecc6a86f0efa44f1694386e961
Chris Kuehl <chris@techxonline.net>
2013-03-18 09:54:30 -0400

    Adding array shuffling library.

---
+++ src/client/ios/www/js/lib/array-shuffle.js
@@ -0,0 +1,11 @@
+// http://stackoverflow.com/a/6274398/450164
+Array.prototype.shuffle = function () {
+    for (var i = this.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = this[i];
+        this[i] = this[j];
+        this[j] = tmp;
+    }
+
+    return this;
+}
