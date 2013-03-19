ee6705ef9e6ce7e65fa7a7555f4aad8f11b1063f
Chris Kuehl <chris@techxonline.net>
2013-03-19 00:52:06 -0400

    Whitespace cleanup.

--- src/server/BrowseRight/grails-app/services/org/browseright/api/category/ContentInterfaceService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/api/category/ContentInterfaceService.groovy
@@ -9,90 +9,90 @@ class ContentInterfaceService {
         
         // groovy.lang.Category exists therefore use full name for our Category
         org.browseright.Category.findAll().each { category ->
-			categories.add([
-					uid: category.uid,
-					title: category.title,
-					shortDescription: category.shortDescription,
-					longDescription: category.longDescription
-				])
+	   categories.add([
+		  uid: category.uid,
+		  title: category.title,
+		  shortDescription: category.shortDescription,
+		  longDescription: category.longDescription
+	       ])
         }
         
         response.categories = categories
     }
     
     def _category = { response, action, params, user, request ->
-		if (! params.uid) {
-			response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
-			response.error = "SPECIFY_UID"
-			return
+        if (! params.uid) {
+	   response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
+	   response.error = "SPECIFY_UID"
+	   return
         }
 		
         def category = Category.findByUid(params.uid)
         
         if (category == null) {
-			response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
-			response.error = "NO_CATEGORY_EXISTS"
-			return
+	   response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
+	   response.error = "NO_CATEGORY_EXISTS"
+	   return
         }
         
         def sections = []
         
         category.sections.each { section ->
-			def sectionData = [
-				uid: section.uid,
-				title: section.title,
-				description: section.description,
-				items: []
-			]
+	   def sectionData = [
+	       uid: section.uid,
+	       title: section.title,
+	       description: section.description,
+	       items: []
+	   ]
 			
-			section.items.each { item ->
-				def isQuiz = (item instanceof Quiz)
-				def itemData = [
-					id: item.id,
-					type: isQuiz ? "QUIZ" : "ARTICLE",
-					title: item.title
-				]
+	   section.items.each { item ->
+	       def isQuiz = (item instanceof Quiz)
+	       def itemData = [
+		  id: item.id,
+		  type: isQuiz ? "QUIZ" : "ARTICLE",
+		  title: item.title
+	       ]
 				
-				sectionData.items.add(itemData)
-			}
+	       sectionData.items.add(itemData)
+	   }
 			
-			sections.add(sectionData)
+	   sections.add(sectionData)
         }
 		
-		response.sections = sections
+        response.sections = sections
     }
 	
-	def _item = { response, action, params, user, request ->
-		if (! params.id) {
-			response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
-			response.error = "SPECIFY_ID"
-			return
+    def _item = { response, action, params, user, request ->
+        if (! params.id) {
+	   response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
+	   response.error = "SPECIFY_ID"
+	   return
         }
 		
-		def item = SectionItem.findById(params.id)
-		def isQuiz = (item instanceof Quiz)
+        def item = SectionItem.findById(params.id)
+        def isQuiz = (item instanceof Quiz)
 		
-		def itemInfo = [
-			id: item.id,
-			type: isQuiz ? "QUIZ" : "ARTICLE",
-			title: item.title
-		]
+        def itemInfo = [
+	   id: item.id,
+	   type: isQuiz ? "QUIZ" : "ARTICLE",
+	   title: item.title
+        ]
 		
-		if (isQuiz) {
-			itemInfo.questionsToShow = item.questionsToShow
-			itemInfo.questions = []
+        if (isQuiz) {
+	   itemInfo.questionsToShow = item.questionsToShow
+	   itemInfo.questions = []
 			
-			item.questions.each { question ->
-				itemInfo.questions.add([
-					text: question.text,
-					correctAnswer: question.correctAnswer,
-					incorrectAnswers: question.incorrectAnswers
-				])
-			}
-		} else {
-			itemInfo.text = item.text
-		}
+	   item.questions.each { question ->
+	       itemInfo.questions.add([
+		      text: question.text,
+		      correctAnswer: question.correctAnswer,
+		      incorrectAnswers: question.incorrectAnswers
+		  ])
+	   }
+        } else {
+	   itemInfo.text = item.text
+        }
 		
-		response.item = itemInfo
-	}
+        response.item = itemInfo
+    }
 }
