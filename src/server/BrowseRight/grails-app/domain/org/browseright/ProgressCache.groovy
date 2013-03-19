651926ebae752294970e5256b02201a8553520ef
Chris Kuehl <chris@techxonline.net>
2013-03-19 19:27:52 -0400

    Now properly calculating best score for each quiz.

--- src/server/BrowseRight/grails-app/domain/org/browseright/ProgressCache.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/ProgressCache.groovy
@@ -13,7 +13,22 @@ class ProgressCache {
 		
 		Quiz.findAll().each { quiz ->
 			def uid = quiz.getFullUID()
-			println uid
+			def bestScore = calculateBestScore(quiz)
+			
+			println "Best score (\"$uid\" (${quiz.id})): $bestScore"
+		}
+	}
+	
+	def calculateBestScore(quiz) {
+		def attempts = QuizAttempt.where {
+			student == student
+			quiz == quiz
+		}.list(sort: "percentCorrect", order: "desc", max: 1)
+		
+		if (attempts.size() <= 0) {
+			return null
+		} else {
+			return attempts[0].percentCorrect
 		}
 	}
 }
