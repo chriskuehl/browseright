9409715e4c566080aa33540fa2d35e8e58775439
Chris Kuehl <chris@techxonline.net>
2013-03-19 17:47:52 -0400

    Whitespace cleanup.

--- src/server/BrowseRight/grails-app/services/org/browseright/api/category/StudentInterfaceService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/api/category/StudentInterfaceService.groovy
@@ -6,57 +6,57 @@ import org.browseright.api.*
 class StudentInterfaceService {
     def create = { response, action, params, user, request ->
         def firstName = params.firstName
-	def lastName = params.lastName
-	def email = params.email
-	def password = params.password
+		def lastName = params.lastName
+		def email = params.email
+		def password = params.password
 	
-	if (password.length() < 5) {
-	    response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
-	    response.error = "PASSWORD_SIZE.TOOSMALL"
-	    return
-	}
+		if (password.length() < 5) {
+			response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
+			response.error = "PASSWORD_SIZE.TOOSMALL"
+			return
+		}
 	
-	def student = new Student(firstName: firstName, lastName: lastName, email: email)
+		def student = new Student(firstName: firstName, lastName: lastName, email: email)
 	
-	student.setPassword(password)
-	student.setLastSeen(request)
-	student.setRegistration(request)
+		student.setPassword(password)
+		student.setLastSeen(request)
+		student.setRegistration(request)
 	
-	if (! student.validate()) {
-	    response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
-	    response.error = "UNKNOWN"
+		if (! student.validate()) {
+			response.apiCode = AppInterface.codes.MISSING_BAD_PARAMS
+			response.error = "UNKNOWN"
             
             student.errors.allErrors.each { error ->
                 response.error = "${error.field}_${error.codes[-1]}".toUpperCase()
             }
-	} else {
-	    student.save()
-	}
+		} else {
+			student.save()
+		}
     }
     
     def login = { response, action, params, user, request ->
-	def email = params.email
-	def password = params.password
+		def email = params.email
+		def password = params.password
 	
-	def student = Student.findByEmail(email)
+		def student = Student.findByEmail(email)
 	
-	if (student == null) {
-	    response.apiCode = AppInterface.codes.BAD_LOGIN_INFO
-	    response.error = "EMAIL"
+		if (student == null) {
+			response.apiCode = AppInterface.codes.BAD_LOGIN_INFO
+			response.error = "EMAIL"
 	    
-	    return
-	}
+			return
+		}
 	
-	if (! student.passwordMatches(password)) {
-	    response.apiCode = AppInterface.codes.BAD_LOGIN_INFO
-	    response.error = "PASSWORD"
+		if (! student.passwordMatches(password)) {
+			response.apiCode = AppInterface.codes.BAD_LOGIN_INFO
+			response.error = "PASSWORD"
 	    
-	    return
-	}
+			return
+		}
 	
-	def token = student.getSessionToken(request)
-	student.save()
+		def token = student.getSessionToken(request)
+		student.save()
 	
-	response.token = token
+		response.token = token
     }
 }
