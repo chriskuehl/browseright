17ea1b0974894c79e6f6026d65fc2d3231b45d4c
Chris Kuehl <chris@techxonline.net>
2013-03-19 17:19:24 -0400

    Adding API logging to stdout.

--- src/server/BrowseRight/grails-app/controllers/org/browseright/api/JSObjectAPIController.groovy
+++ src/server/BrowseRight/grails-app/controllers/org/browseright/api/JSObjectAPIController.groovy
@@ -10,7 +10,18 @@ class JSObjectAPIController {
         def category = params.apiCategory
         def item = params.apiItem
         def action = params.apiAction
-        
+		
+		def str = "[" + (new Date()).toString() + "]: \""
+        str += category + "/" + item
+		
+		if (action) {
+			str += "/" + action
+		}
+		
+		str += "\" " + request.getRemoteAddr()
+		
+		println str
+		
         def response = appInterfaceService.generateResponse(request, category, item, action, params)
         
         if (params.xml) {
