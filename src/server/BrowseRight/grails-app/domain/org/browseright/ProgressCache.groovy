2ac483cecd003ba82a84b4bc2c05a6293398ad04
Chris Kuehl <chris@techxonline.net>
2013-03-19 19:04:05 -0400

    Starting to add cache update methods.

--- src/server/BrowseRight/grails-app/domain/org/browseright/ProgressCache.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/ProgressCache.groovy
@@ -1,7 +1,19 @@
 package org.browseright
 
 class ProgressCache {
-    // TODO: this class
     static constraints = {
+		bestScores(nullable: false)
     }
+	static belongsTo = [student: Student]
+	
+	Map bestScores
+	
+	def recalculateProgress() {
+		bestScores = [:]
+		
+		Quiz.findAll().each { quiz ->
+			def uid = quiz.getFullUID()
+			println uid
+		}
+	}
 }
