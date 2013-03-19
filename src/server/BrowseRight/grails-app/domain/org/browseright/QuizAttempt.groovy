0e71dda57f2e80487044bed4f40e9677bd1b60b9
Chris Kuehl <chris@techxonline.net>
2013-03-19 19:16:21 -0400

    Whitespace cleanup.

--- src/server/BrowseRight/grails-app/domain/org/browseright/QuizAttempt.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/QuizAttempt.groovy
@@ -17,20 +17,20 @@ class QuizAttempt {
     int quizType
     
     int getNumberCorrect() {
-	int correct = 0
+		int correct = 0
 	
-	questions.each { question ->
-	    if (question.isCorrect()) {
-		correct ++
-	    }
-	}
+		questions.each { question ->
+			if (question.isCorrect()) {
+				correct ++
+			}
+		}
 	
-	correct
+		correct
     }
     
     double getPercentCorrect() {
         if (questions == null || questions.size() <= 0) {
-	   return 0 // infinity
+			return 0 // infinity
         }
         
         ((double) getNumberCorrect()) / ((double) questions.size())
