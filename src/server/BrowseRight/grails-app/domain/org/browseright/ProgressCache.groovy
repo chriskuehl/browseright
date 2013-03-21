c4d9aba42a6d4f5d8cb4e32ed88edcd2619e218a
Chris Kuehl <chris@techxonline.net>
2013-03-21 10:38:06 -0400

    Now calculating per-category and total progress.

--- src/server/BrowseRight/grails-app/domain/org/browseright/ProgressCache.groovy
+++ src/server/BrowseRight/grails-app/domain/org/browseright/ProgressCache.groovy
@@ -7,9 +7,12 @@ class ProgressCache {
 	static belongsTo = [student: Student]
 	static hasMany = [quizzes: QuizScore]
 	
+	double totalProgress = 0
+	Map categories = [:]
 	Map quizzes = [:]
 	
 	def recalculateProgress() {
+		// calculate best score for each quiz
 		Quiz.findAll().each { quiz ->
 			def uid = quiz.getFullUID()
 			def bestScore = calculateBestScore(quiz)
@@ -23,6 +26,49 @@ class ProgressCache {
 				println "No attempt found for \"$uid\" (${quiz.id})"
 			}
 		}
+		
+		// calculate progress for each category
+		def sum = 0
+		def n = 0	
+		
+		org.browseright.Category.findAll().each { category ->
+			sum += calculateProgressForCategory(category)
+			n ++
+		}
+		
+		totalProgress = (sum / n)
+		println "Total progress: ${totalProgress}"
+	}
+	
+	def calculateProgressForCategory(category) {
+		// get a list of quizzes for the category
+		def quizzes = []
+		
+		category.sections.findAll().each { section ->
+			section.items.findAll().each { item ->
+				if (item instanceof Quiz) {
+					quizzes.add(item)
+				}
+			}
+		}
+		
+		// see how many quizzes they've passed
+		def passed = 0
+		
+		quizzes.each { quiz ->
+			def uid = quiz.getFullUID()
+			
+			if (hasCompletedQuiz(uid)) {
+				passed ++
+			}
+		}
+		
+		def percent = quizzes.size() <= 0 ? 0 : (passed / quizzes.size())
+		categories[category.uid] = percent.toString()
+		
+		println "Category progress ($category): ${passed}/${quizzes.size()} (${percent * 100}%)"
+
+		return percent
 	}
 	
 	def calculateBestScore(quiz) {
