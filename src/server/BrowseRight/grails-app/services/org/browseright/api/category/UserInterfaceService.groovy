303f648667b81b0348bbfe7cc8e9691a593f3aa8
Chris Kuehl <chris@techxonline.net>
2013-03-21 10:37:57 -0400

    Whitespace cleanup.

--- src/server/BrowseRight/grails-app/services/org/browseright/api/category/UserInterfaceService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/api/category/UserInterfaceService.groovy
@@ -6,36 +6,36 @@ import org.browseright.api.*
 class UserInterfaceService {
     def _info = { response, action, params, user, request ->
         def info = [
-	   firstName: user.firstName,
-	   lastName: user.lastName,
-	   email: user.email,
-	   lastPasswordChangeTime: user.lastPasswordChangeTime
+			firstName: user.firstName,
+			lastName: user.lastName,
+			email: user.email,
+			lastPasswordChangeTime: user.lastPasswordChangeTime
         ]
 	
         if (user.school) {
-	   info.school = [
-	       name: user.school.name,
-	       street: user.school.street,
-	       city: user.school.city,
-	       zipCode: user.school.zipCode,
-	       state: user.school.state,
-	       latitude: user.school.latitude,
-	       longitude: user.school.longitude,
-	       quizPassThreshold: user.school.quizPassThreshold,
-	       reviewPassThreshold: user.school.reviewPassThreshold,
-	       questionsPerReview: user.school.questionsPerReview,
-	       helpEmail: user.school.helpEmail,
-	       announcementText: user.school.announcementText,
-	       helpText: user.school.helpText,
-	       teachers: []
-	   ]
+			info.school = [
+				name: user.school.name,
+				street: user.school.street,
+				city: user.school.city,
+				zipCode: user.school.zipCode,
+				state: user.school.state,
+				latitude: user.school.latitude,
+				longitude: user.school.longitude,
+				quizPassThreshold: user.school.quizPassThreshold,
+				reviewPassThreshold: user.school.reviewPassThreshold,
+				questionsPerReview: user.school.questionsPerReview,
+				helpEmail: user.school.helpEmail,
+				announcementText: user.school.announcementText,
+				helpText: user.school.helpText,
+				teachers: []
+			]
 	   
-	   user.school.teachers.each { teacher ->
-	       info.school.teachers.add([
-		      firstName: teacher.firstName,
-		      lastName: teacher.lastName
-	       ])
-	   }
+			user.school.teachers.each { teacher ->
+				info.school.teachers.add([
+						firstName: teacher.firstName,
+						lastName: teacher.lastName
+					])
+			}
         }
         
         response.info = info
