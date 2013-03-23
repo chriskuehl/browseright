214d00ee258c284992e13bc73a374e3df2009798
Chris Kuehl <chris@techxonline.net>
2013-03-23 18:04:47 -0400

    Using MySQL in production.

--- src/server/BrowseRight/grails-app/conf/DataSource.groovy
+++ src/server/BrowseRight/grails-app/conf/DataSource.groovy
@@ -19,10 +19,34 @@ environments {
         }
     }
     
-    production {
+    production { // vZUCQQpQsenNxGzc
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:h2:mem:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "update"
+			
+			// TODO: move this stuff to an external config file (and change the password!)
+            url = "jdbc:mysql://localhost/browsright?useUnicode=yes&characterEncoding=UTF-8"
+            username = "browseright"
+            password = "vZUCQQpQsenNxGzc"
+			
+			// misc options
+			pooled = true
+			driverClassName = "com.mysql.jdbc.Driver"
+			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+
+			// resolve an issue with stale connections and Grails due to automatic
+			// connection pooling done by Grails (enabled above)
+			// http://stackoverflow.com/questions/2740987/mysql-connection-timeout-issue-grails-application-on-tomcat-using-hibernate-an
+
+			// run the evictor every 30 minutes and evict any connections older than 30 minutes.
+			minEvictableIdleTimeMillis = 1800000
+			timeBetweenEvictionRunsMillis = 1800000
+			numTestsPerEvictionRun = 3
+
+			// test the connection while its idle, before borrow and return it
+			testOnBorrow = true
+			testWhileIdle = true
+			testOnReturn = true
+			validationQuery = "SELECT 1"
         }
     }
 }
