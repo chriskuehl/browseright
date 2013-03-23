f90098ed64565ed0d6bd7dd7d68bf253e37c2513
Chris Kuehl <chris@techxonline.net>
2013-03-23 16:51:52 -0400

    Don't allow completion of non-ready sections.

--- src/client/ios/www/screens/lesson/lesson/lesson.js
+++ src/client/ios/www/screens/lesson/lesson/lesson.js
@@ -76,6 +76,7 @@ function addSectionLesson(section, sectionIndex) {
 		
 		if (item.completed) {
 			li.addClass("completed");
+			li.addClass("ready");
 		}
 		
 		if (item.type == "ARTICLE") {
@@ -89,7 +90,7 @@ function addSectionLesson(section, sectionIndex) {
 		});
 		
 		li.click(function() {
-			if ($(this).hasClass("disabled") || (currentItem[0] == $(this).data("sectionIndex") && currentItem[1] == $(this).data("itemIndex"))) {
+			if (! $(this).hasClass("ready") || (currentItem[0] == $(this).data("sectionIndex") && currentItem[1] == $(this).data("itemIndex"))) {
 				return;
 			}
 			
