9220005ff81901d9b85a39539bd4073a8701aea1
Chris Kuehl <chris@techxonline.net>
2013-03-23 16:35:42 -0400

    Display reputation category progress.

--- src/client/ios/www/screens/user/progress/progress.js
+++ src/client/ios/www/screens/user/progress/progress.js
@@ -5,13 +5,38 @@ gui.screens["user/progress"].data = {
 	}],
 
 	setup: function(contentManager) {
-		// TODO: use contentManager for this
-		// TODO: incorporate JS magic tokens
-		registerScrollContainers($(".scroll"));
-		
 		// load user information
 		loadStudentData(function() {
 			updateTotalProgress();
+			
+			// TODO: make this work with all categories
+			apiWithLoading("Loading progress...", "content/category", {uid: "reputation"}, [RESP_OK], function(code, data) {
+				var list = $(".progress.reputation");
+				
+				for (var i = 0; i < data.sections.length; i ++) {
+					var section = data.sections[i];
+					var li = $("<li />");
+					li.text(section.title);
+					li.addClass("section");
+					li.appendTo(list);
+					
+					for (var j = 0; j < section.items.length; j ++) {
+						var item = section.items[j];
+						
+						var li2 = $("<li />");
+						li2.text(item.title);
+						
+						if (item.completed) {
+							li2.addClass("completed");
+						}
+						
+						li2.appendTo(list);
+					}
+				}
+				
+				
+				registerScrollContainers($(".scroll"));
+			});
 		});
 	}
 };
