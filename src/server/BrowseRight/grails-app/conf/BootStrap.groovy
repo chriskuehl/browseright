fbb58651738debe800ddef65c63db955766a2ea0
Chris Kuehl <chris@techxonline.net>
2013-03-23 18:09:27 -0400

    No longer running a bunch of API commands on bootstrap.

--- src/server/BrowseRight/grails-app/conf/BootStrap.groovy
+++ src/server/BrowseRight/grails-app/conf/BootStrap.groovy
@@ -10,6 +10,7 @@ class BootStrap {
         contentService.initContentDatabase()
 	
         // execute some API methods to add some data for testing
+		/*
         appInterfaceService.generateResponse(null, "teacher", "create", null, [firstName: "Some", lastName: "Teacher", email: "teacher.test@browseright.org", password: "teacher"])
         appInterfaceService.generateResponse(null, "student", "create", null, [firstName: "Some", lastName: "Student", email: "student.test@browseright.org", password: "student"])
 	
@@ -27,7 +28,8 @@ class BootStrap {
         // create school and add student
         appInterfaceService.generateResponse(null, "school", "create", null, [token: "1" * 255, name: "Woodford County High School", street: "180 Frankfort Street", city: "Versailles", zipCode: "40383", helpEmail: "becky.keith@woodford.kyschools.us"])
         appInterfaceService.generateResponse(null, "user", "joinSchool", null, [token: "0" * 255, school: 1])
-    }
+		*/
+	}
     
     def destroy = {
     }
