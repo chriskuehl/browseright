5e09d5075cd84917866fcf6c9a931e5e0b8e1e5e
Chris Kuehl <chris@techxonline.net>
2013-03-29 14:40:07 -0400

    Using YouTube embedding instead of local embedding again.

--- src/client/ios/www/js/app/markdown.js
+++ src/client/ios/www/js/app/markdown.js
@@ -3,8 +3,8 @@ var markdownConverter = Markdown.getSanitizingConverter();
 function markdown(src) {
 	src = src.replace(/\\n/g, "\n");
 	src = markdownConverter.makeHtml(src);
-	//src = src.replace(/\[YOUTUBE:(.*?)\]/g, "<iframe width=\"1280\" height=\"720\" src=\"http://www.youtube.com/embed/$1\" frameborder=\"0\" allowfullscreen></iframe>");
-	src = src.replace(/\[YOUTUBE:(.*?)\]/g, "<video width=\"1280\" height=\"720\" controls><source src=\"videos/$1.mp4\" type='video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"'></video>")
+	src = src.replace(/\[YOUTUBE:(.*?)\]/g, "<iframe width=\"1280\" height=\"720\" src=\"http://www.youtube.com/embed/$1\" frameborder=\"0\" allowfullscreen></iframe>");
+	//src = src.replace(/\[YOUTUBE:(.*?)\]/g, "<video width=\"1280\" height=\"720\" controls><source src=\"videos/$1.mp4\" type='video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"'></video>")
 		
 	return src;
 }
