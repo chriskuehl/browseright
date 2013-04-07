128ae294ced2c8639760f41274a4a2209d6e0cde
Chris Kuehl <chris@techxonline.net>
2013-04-06 22:19:18 -0400

    Loading production database info (username, password, JDBC URL) from environment variables instead of a config file.

--- src/server/BrowseRight/grails-app/conf/DataSource.groovy
+++ src/server/BrowseRight/grails-app/conf/DataSource.groovy
@@ -1,9 +1,3 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -14,46 +8,24 @@ hibernate {
 environments {
     development {
         dataSource {
-            //dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 			
-			dbCreate = "update"
-			
-			// TODO: move this stuff to an external config file (and change the password!)
-            url = "jdbc:mysql://localhost/browseright?useUnicode=yes&characterEncoding=UTF-8"
-            username = "root"
-            password = "" // lol
-			
-			// misc options
 			pooled = true
-			driverClassName = "com.mysql.jdbc.Driver"
-			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-
-			// resolve an issue with stale connections and Grails due to automatic
-			// connection pooling done by Grails (enabled above)
-			// http://stackoverflow.com/questions/2740987/mysql-connection-timeout-issue-grails-application-on-tomcat-using-hibernate-an
-
-			// run the evictor every 30 minutes and evict any connections older than 30 minutes.
-			minEvictableIdleTimeMillis = 1800000
-			timeBetweenEvictionRunsMillis = 1800000
-			numTestsPerEvictionRun = 3
-
-			// test the connection while its idle, before borrow and return it
-			testOnBorrow = true
-			testWhileIdle = true
-			testOnReturn = true
-			validationQuery = "SELECT 1"
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
+			
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
         }
     }
     
-    production { // vZUCQQpQsenNxGzc
+    production {
         dataSource {
             dbCreate = "update"
 			
-			// TODO: move this stuff to an external config file (and change the password!)
-            url = "jdbc:mysql://localhost/browseright?useUnicode=yes&characterEncoding=UTF-8"
-            username = "browseright"
-            password = "vZUCQQpQsenNxGzc"
+            url = System.env.JDBC_URL
+            username = System.env.JDBC_USERNAME
+            password = System.env.JDBC_PASSWORD
 			
 			// misc options
 			pooled = true
