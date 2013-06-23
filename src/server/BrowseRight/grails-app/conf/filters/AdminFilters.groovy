afe195b2f28ad83b52d937611b80626bd9c332db
Chris Kuehl <chris@techxonline.net>
2013-06-22 23:56:04 -0400

    Support bcrypt-hashed passwords for admin accounts (fixes #20).

--- src/server/BrowseRight/grails-app/conf/filters/AdminFilters.groovy
+++ src/server/BrowseRight/grails-app/conf/filters/AdminFilters.groovy
@@ -6,6 +6,7 @@ import grails.util.GrailsUtil
 class AdminFilters {
     def request
 	def response
+	def hashingService
     
     def filters = {
         adminFilter(uri: "/dev/**") {
@@ -20,8 +21,12 @@ class AdminFilters {
 						def raw =  new String(new sun.misc.BASE64Decoder().decodeBuffer(b64));  
 						def parts = raw.split(':')
 						
-						if (parts.size() == 2 && parts[0].length() > 0 && parts[1].length() > 0 && System.env["BROWSERIGHT_ADMIN_" + parts[0].toUpperCase()] == parts[1]) {
-							success = true
+						if (parts.size() == 2 && parts[0].length() > 0 && parts[1].length() > 0) {
+							def hash = System.env["BROWSERIGHT_ADMIN_" + parts[0].toUpperCase()]
+							
+							if (hash && hash.length > 0 && hashingService.matches(hash, parts[1])) {
+								success = true
+							}
 						}
 					}	
 					
