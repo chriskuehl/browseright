a41bb9ed63636b00e9170f14c6ebd1a12066631d
Chris Kuehl <chris@techxonline.net>
2013-11-04 23:10:44 -0800

    Remove outdated comments about number of rounds of bcrypt.

--- src/server/BrowseRight/grails-app/services/org/browseright/HashingService.groovy
+++ src/server/BrowseRight/grails-app/services/org/browseright/HashingService.groovy
@@ -4,8 +4,6 @@ import org.mindrot.jbcrypt.BCrypt
 
 class HashingService {
     def hash(def str) {
-        // 6 rounds takes about a second on my development machine; additional
-        // benchmarking and testing is needed before we go live
         BCrypt.hashpw(str, BCrypt.gensalt(9))
     }
     
