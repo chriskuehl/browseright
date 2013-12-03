2c8c5b8d73847a9da037f9e9862c82e7f55d9879
Chris Kuehl <chris@techxonline.net>
2013-12-02 16:23:04 -0800

    Use Apache commons for base64 decoding.
    Fixes #18.

--- src/server/BrowseRight/grails-app/conf/filters/AdminFilters.groovy
+++ src/server/BrowseRight/grails-app/conf/filters/AdminFilters.groovy
@@ -1,5 +1,7 @@
 package filters
 
+import org.apache.commons.codec.binary.Base64
+import org.apache.commons.codec.binary.StringUtils
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import grails.util.GrailsUtil
 
@@ -11,14 +13,14 @@ class AdminFilters {
     def filters = {
         adminFilter(uri: "/dev/**") {
             before = {
-                if (GrailsUtil.getEnvironment().equals(GrailsApplication.ENV_PRODUCTION)) {
+                if (GrailsUtil.getEnvironment().equals(GrailsApplication.ENV_PRODUCTION) || System.env["BROWSERIGHT_RESTRICT_ADMIN"]) {
                     // check the provided username and password
 					def success = false
 					def auth = request.getHeader("Authorization")
 					
 					if (auth) {
 						def b64 = auth - 'Basic '  
-						def raw =  new String(new sun.misc.BASE64Decoder().decodeBuffer(b64));  
+						def raw =  StringUtils.newStringUtf8(Base64.decodeBase64(b64))
 						def parts = raw.split(':')
 						
 						if (parts.size() == 2 && parts[0].length() > 0 && parts[1].length() > 0) {
